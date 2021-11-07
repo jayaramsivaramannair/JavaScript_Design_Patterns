61606789aea02405b4c34033272eb59f114a9619
Jayaram Nair <jayaramsivaramannair@gmail.com>
2021-11-07 16:48:30 -0500

    Added a fetchData function to movies from OMDB api

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hi there!');
+const fetchData = async () => {
+  const response = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apikey: '4bbade0b',
+      s: 'avengers'
+    }
+  });
+
+  console.log(response.data);
+}
+
+fetchData();
