35e4f60c48e9bf37d259b6a2bbb3478105ee6655
Jayaram Nair <jayaramsivaramannair@gmail.com>
2021-11-07 17:40:13 -0500

    Added a set timeout function to prevent frequent hits to the API

--- index.js
+++ index.js
@@ -5,11 +5,27 @@ const fetchData = async (searchTerm) => {
       s: searchTerm,
     }
   });
-
   console.log(response.data);
 }
 
+
 const input = document.querySelector('input');
-input.addEventListener('input', (event) => {
-  console.log(event.target.value);
-})
+
+let timeoutId;
+
+//This is the call back function which will be passed into the event listener on input
+const onInput = event => {
+  //While entering the function, if the timerID is alraeady set then clear it and set a new timer
+  if(timeoutId) {
+    clearTimeout(timeoutId);
+  }
+
+  //This is the timeout function which will fetch movies after 1000 milliseconds has elapsed
+  //Once the user stops typing, the final timer will run after 1000 milliseconds and fetch the movies
+  timeoutId = setTimeout(() => {
+    fetchData(event.target.value);
+  }, 1000)
+};
+
+
+input.addEventListener('input', /* onInput */);
