9475198c37f11448aa412e79965a88c0fc489e19
Jayaram Nair <jayaramsivaramannair@gmail.com>
2021-11-07 18:06:43 -0500

    Added a debounce function to abstract away callback passed to event listener

--- index.js
+++ index.js
@@ -11,21 +11,29 @@ const fetchData = async (searchTerm) => {
 
 const input = document.querySelector('input');
 
-let timeoutId;
+//Debounce is a function which returns a function.
+const debounce = (cb, delay = 500) => {
+  let timeoutId;
+  return (...args) => {
+    //While entering the function, if the timerID is already set then clear it and set a new timer
+    if(timeoutId) {
+      clearTimeout(timeoutId);
+    }
+
+    //This is the timeout function which will fetch movies after 1000 milliseconds has elapsed
+    //Once the user stops typing, the final timer will run after 1000 milliseconds and fetch the movies
+    timeoutId = setTimeout(() => {
+      //Take each argument passed into the function and pass them separately into the callback function
+      cb.apply(null, args);
+    }, delay)
+  };
+}
+
 
 //This is the call back function which will be passed into the event listener on input
-const onInput = event => {
-  //While entering the function, if the timerID is alraeady set then clear it and set a new timer
-  if(timeoutId) {
-    clearTimeout(timeoutId);
-  }
-
-  //This is the timeout function which will fetch movies after 1000 milliseconds has elapsed
-  //Once the user stops typing, the final timer will run after 1000 milliseconds and fetch the movies
-  timeoutId = setTimeout(() => {
+const onInput = (event) => {
     fetchData(event.target.value);
-  }, 1000)
 };
 
 
-input.addEventListener('input', /* onInput */);
+input.addEventListener('input', debounce(onInput, 1000));
