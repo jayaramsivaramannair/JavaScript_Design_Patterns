26cbe3e4d9f9ea2ee7818fdc0a97f3a4cb92f9cc
Jayaram Nair <jayaramsivaramannair@gmail.com>
2021-11-11 07:18:43 -0500

    Wrapped up config object passed to the function for creating auto-complete widget

--- index.js
+++ index.js
@@ -1,30 +1,36 @@
 
-  const fetchData = async (searchTerm) => {
-    const response = await axios.get('http://www.omdbapi.com/', {
-      params: {
-        apikey: '4bbade0b',
-        s: searchTerm,
+  createAutoComplete({
+    root: document.querySelector('.autocomplete'),
+    renderOption(movie) {
+      const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
+      return `
+        <img src="${imgSrc}"/>
+        ${movie.Title} (${movie.Year})
+        `
+    },
+    onOptionSelect(movie) {
+      onMovieSelect(movie);
+    },
+
+    inputValue(movie) {
+      return movie.Title;
+    },
+
+    async fetchData (searchTerm) {
+      const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+          apikey: '4bbade0b',
+          s: searchTerm,
+        }
+      });
+      
+      //If the search does not yield any results, then simply return an empty array
+      if(response.data.Error) {
+        return [];
       }
-    });
-  
-    //If the search does not yield any results, then simply return an empty array
-    if(response.data.Error) {
-      return [];
+      
+      return response.data.Search;
     }
-  
-    return response.data.Search;
-  }
-
-  createAutoComplete({
-    root: document.querySelector('.autocomplete')
-  });
-
-  createAutoComplete({
-    root: document.querySelector('.autocomplete-two')
-  });
-
-  createAutoComplete({
-    root: document.querySelector('.autocomplete-three')
   });
 
   const onMovieSelect = async (movie) => {
