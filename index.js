36eba3eda55813d670a2b7f14b4b96f1a99e8612
Jayaram Nair <jayaramsivaramannair@gmail.com>
2021-11-11 07:32:58 -0500

    Altered HTML to show two auto-complete widgets to show two instead of one

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-
-  createAutoComplete({
-    root: document.querySelector('.autocomplete'),
+  const autoCompleteConfig = {
     renderOption(movie) {
       const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
       return `
@@ -31,6 +29,17 @@
       
       return response.data.Search;
     }
+  }
+  
+  
+  createAutoComplete({
+    ...autoCompleteConfig,
+    root: document.querySelector('#left-autocomplete'),
+  });
+
+  createAutoComplete({
+    ...autoCompleteConfig,
+    root: document.querySelector('#right-autocomplete'),
   });
 
   const onMovieSelect = async (movie) => {
